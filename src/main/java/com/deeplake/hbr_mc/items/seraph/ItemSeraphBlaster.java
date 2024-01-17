f823e6652e8b74bcb3432f0acc5561709379870f
TaoismDeepLake <pymess@icloud.com>
2024-01-17 23:16:52 +0800

    text work

---
+++ src/main/java/com/deeplake/hbr_mc/items/seraph/ItemSeraphBlaster.java
@@ -0,0 +1,29 @@
+package com.deeplake.hbr_mc.items.seraph;
+
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+
+import javax.annotation.Nullable;
+import java.util.Set;
+
+public class ItemSeraphBlaster extends ItemSeraphBase{
+    public ItemSeraphBlaster(String name) {
+        super(name);
+    }
+
+    @Override
+    public float getDestroySpeed(ItemStack stack, IBlockState state) {
+        return 3.0F;
+    }
+
+    @Override
+    public int getHarvestLevel(ItemStack stack, String toolClass, @Nullable EntityPlayer player, @Nullable IBlockState blockState) {
+        return 0;
+    }
+
+    @Override
+    public Set<String> getToolClasses(ItemStack stack) {
+        return super.getToolClasses(stack);
+    }
+}
