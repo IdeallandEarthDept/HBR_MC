da5ef3ee7713daa7b7f58b17204d53d1f4b6fb5a
TaoismDeepLake <pymess@icloud.com>
2024-03-10 17:23:01 +0800

    milestone

--- src/main/java/com/deeplake/hbr_mc/Main.java
+++ src/main/java/com/deeplake/hbr_mc/Main.java
@@ -18,7 +18,7 @@ public class Main
 {
     public static final String MODID = "hbr_mc";
     public static final String NAME = "Heaven Burns Red";
-    public static final String VERSION = "1.1.4";
+    public static final String VERSION = "1.1.5";
 
     private static Logger logger;
     @Mod.Instance
