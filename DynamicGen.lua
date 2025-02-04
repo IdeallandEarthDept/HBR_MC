cdc613950d3a95e1955086749acc6e715c4e188d
TaoismDeepLake <pymess@icloud.com>
2025-02-04 15:14:36 +0800

    Imported those instruments and the bed

--- DynamicGen.lua
+++ DynamicGen.lua
@@ -8,12 +8,12 @@ require "GenBlockJson"
 
 GenBlock("inst_microphone_seated");
 GenBlock("inst_microphone_in_hand");
---GenBlock("inst_st_guitar");
---GenBlock("inst_bed_double");
---GenBlock("inst_microphone");
---GenBlock("inst_drum_set");
---GenBlock("inst_wood_guitar");
---GenBlock("inst_bass");
---GenBlock("inst_keyboard");
---GenBlock("inst_speaker");
+GenBlock("inst_st_guitar");
+GenBlock("inst_bed_double");
+GenBlock("inst_microphone");
+GenBlock("inst_drum_set");
+GenBlock("inst_wood_guitar");
+GenBlock("inst_bass");
+GenBlock("inst_keyboard");
+GenBlock("inst_speaker");
 
--- src/main/java/com/deeplake/hbr_mc/init/RegisterBlocks.java
+++ src/main/java/com/deeplake/hbr_mc/init/RegisterBlocks.java
@@ -24,10 +24,11 @@ public class RegisterBlocks {
     public static final Block ENERGY_ORE = new BlockBase("energy_ore", Material.ROCK).setHardness(15f);
     public static final Block CANCER_FLOOR = new BlockCarpetBase("cancer_carpet",Material.IRON).setHardness(4f).setResistance(999f);
 
-    public static final Block CANCER_CARPET = new BlockInstrument("cancer_carpet");
     public static final Block INST_ST_GUITAR = new BlockInstrument("inst_st_guitar");
     public static final Block INST_BED_DOUBLE = new BlockInstrument("inst_bed_double");
     public static final Block INST_MICROPHONE = new BlockInstrument("inst_microphone");
+    public static final Block INST_MICROPHONE_SEATED = new BlockInstrument("inst_microphone_seated");
+    public static final Block INST_MICROPHONE_IN_HAND = new BlockInstrument("inst_microphone_in_hand");
     public static final Block INST_DRUM_SET = new BlockInstrument("inst_drum_set");
     public static final Block INST_WOOD_GUITAR = new BlockInstrument("inst_wood_guitar");
     public static final Block INST_BASS = new BlockInstrument("inst_bass");
